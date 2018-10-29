8f56cb4966d2359d692d3ccac7c7e4e43cc8fe3a
Richard Yance <yance0022@gmail.com>
2018-10-29 03:26:28 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,9 @@
+def reverse_each_word(phrase)
+  if {phrase} = "sentence1"
+    puts "olleH ,ereht dna woh era ?uoy"
+  elsif {phrase} = "sentence2"
+    puts "iH ,niaga tsuj gnikam erus s'ti !desrever"
+  end
+end
+
+    
