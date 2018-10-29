2f4ceb873311d831d8d881b8bb2f40f1027f2d16
Richard Yance <yance0022@gmail.com>
2018-10-29 03:25:09 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(phrase)
-  if phrase = "sentence1"
+  if {phrase} = "sentence1" 
     puts "olleH ,ereht dna woh era ?uoy"
-  elsif phrase = "sentence2"
+  elsif {phrase} = "sentence2"
     puts "iH ,niaga tsuj gnikam erus s'ti !desrever"
   end
 end
